f35b3c685d45924d5be908022debf7f8d3d7aa10
Bwalya Nonde <bwalyanonde8@gmail.com>
2022-05-12 10:34:19 -0400

    Update script.js.file

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  openNav ();
-
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
-  closeNav ();
+  document.getElementById('myNav').style.width = '100%';
+}
+openNav();
+function closeNav() {
+  document.getElementById('myNav').style.width = '0%';
+}
+closeNav();
