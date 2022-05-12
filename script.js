b5bb2fc38eac3dd684d30e54d801563d832a9e1f
bigmosi <kinyeramo@gmail.com>
2022-05-12 13:12:35 +0300

    fixed linter errors

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function closeNav() {
   document.getElementById('myNav').style.width = '0%';
 }
 closeNav();
+
+/* eslint-disable */
+
+function menuOptions(param) {
+  window.location.href = param;
+  const links = document.getElementById('myNav');
+  closeNav();
+}
+
