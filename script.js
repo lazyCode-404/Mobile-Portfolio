acb2964142306076e26176476872f06d94d892c8
Bwalya Nonde <bwalyanonde8@gmail.com>
2022-05-12 20:16:58 -0400

    Add form validation code to script.js file

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ function menuOptions(param) {
   closeNav();
 }
 
+// form validation
+const form = document.querySelector('.form-text');
+const emailInput = form.elements.email;
+
+const INPUT_LOWERCASE = 'Error : Please enter email a lowercase input';
+
+function showMessage(input, message, type) {
+  const msg = document.querySelector('.error-text');
+  msg.innerText = message;
+  input.className = type ? 'success' : 'error';
+  return type;
+}
+
+function showError(input, message) {
+  return showMessage(input, message, false);
+}
