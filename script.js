53526fcb22074f5e1629fbcfc845cdafb4d71b61
Bwalya Nonde <bwalyanonde8@gmail.com>
2022-05-17 19:01:38 -0400

    Add form validation to portfolio

--- script.js
+++ script.js
@@ -15,4 +15,28 @@ function menuOptions(param) {
   closeNav();
 }
 
+// form validation
+const form = document.querySelector('.conct-form');
+const emailInput = form.elements.email;
+
+const INPUT_LOWERCASE = 'Error : Please enter a lowercase email input';
+
+function showMessage(input, message, type) {
+  const msg = document.querySelector('.error-text');
+  msg.innerText = message;
+  input.className = type ? 'success' : 'error';
+  return type;
+}
+
+function showError(input, message) {
+  return showMessage(input, message, false);
+}
+
+function validateEmail(input, invalidLowercase) {
+  if (input.value === input.value.toLowerCase()) {
+    return true;
+  }
+  return showError(input, invalidLowercase);
+}
+
 
