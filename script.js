a9b1508070621ba5e589cd1a0c14e86540d07abc
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-05-17 10:29:04 +0100

    Add JS function to the file

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-
+function openNav() {
+    document.getElementById('myNav').style.width = '100%';
+  }
+  openNav();
+  function closeNav() {
+    document.getElementById('myNav').style.width = '0%';
+  }
+  closeNav();
 
