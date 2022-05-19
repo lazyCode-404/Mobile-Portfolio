a981018c609cd9af29cdb3c5c676c4e5d2d108f5
Bwalya Nonde <bwalyanonde8@gmail.com>
2022-05-19 15:51:13 -0400

    Update main.js file

--- JS/main.js
+++ JS/main.js
@@ -112,15 +112,6 @@ const openbtn = document.querySelectorAll('.button');
 const closebtn = document.querySelector('.close-btn');
 // listen for click
 
-// display popup
-function openModal() {
-  circle1.classList.replace('show', 'hide');
-  circle2.classList.replace('show', 'hide');
-  popUp.classList.add('active');
-  if (popUp.classList.contains('active')) {
-    document.querySelector('body').style.overflow = 'hidden';
-  }
-}
 
 // close popup
 function closeModal() {
